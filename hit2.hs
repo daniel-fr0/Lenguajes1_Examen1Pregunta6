5b8c1bee8ace6b4e269480920395fa9418c46c49
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2023-10-18 03:57:29 -0400

    smallest haskell

--- hit2.hs
+++ hit2.hs
@@ -5,9 +5,7 @@ c n k=p n/(p k*p(n-k))
 f n=h n 0 1
 h 0 a b=a
 h n a b=h(n-1)b(a+b)
-n x y=c x y*c x (y-1)/x
-l=logBase 2
-w x=f(truncate(l(n(x+1)(x-1)))+1)
+w n=f(floor(logBase 2(c n (n-2)*c n (n-3)/n))+1)
 main=do
- args<-getArgs
- print(w(read(head args)))
+ a<-getArgs
+ print(w(read(head a)+1))
