ace7d5bd65ee7e9a63e23daea53ca16efcf566e7
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2023-10-18 03:22:17 -0400

    hit2 ya funciona

--- hit2.hs
+++ hit2.hs
@@ -2,13 +2,12 @@ import System.Environment
 p 0=1
 p n=n*p(n-1)
 c n k=p n/(p k*p(n-k))
-f 0=0
-f 1=1
-f n=f(n-1)+f(n-2)
-n x y=c x y/c x (y-1)/x
+f n=h n 0 1
+h 0 a b=a
+h n a b=h(n-1)b(a+b)
+n x y=c x y*c x (y-1)/x
 l=logBase 2
-w x=f(truncate(l(n (x+1)(x-1)))+1)
-main::IO()
+w x=f(truncate(l(n(x+1)(x-1)))+1)
 main=do
- a<-getArgs
- print $ w $ read $ head a
+ args<-getArgs
+ print(w(read(head args)))
