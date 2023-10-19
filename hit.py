8aa504b1b242529b63a105e956eaa2eeae88a55c
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2023-10-19 01:00:08 -0400

    148 chars

--- hit.py
+++ hit.py
@@ -1,5 +1,5 @@
 import math,sys
 C=math.comb
-F=lambda n,a=0,b=1:n<1 and a or F(n-1,b,a+b)
+F=lambda n,a=0,b=1:n and F(n-1,b,a+b)or a
 W=lambda n:F((C(n,n-2)*C(n,n-3)//n).bit_length())
 print(W(int(sys.argv[1])+1))
